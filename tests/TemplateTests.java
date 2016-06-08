e9fd5daf855827dbdfbaaf4fd0979bfdd08bcbcf
Pierre-Alexandre VEYRY <veyry_p@epita.fr>
2016-06-08 13:17:24 +0200

    tests: add junit to maven config file and first test template class

---
+++ tests/TemplateTests.java
@@ -0,0 +1,31 @@
+import org.junit.*;
+
+public class TemplateTests {
+
+    @BeforeClass
+    public static void initClass() throws Exception {
+        // Executed before the first tests of the class
+    }
+
+    @AfterClass
+    public static void finishClass() throws Exception {
+        // Executed after all the tests of the class
+    }
+
+    @Before
+    public void initTest() throws Exception {
+        // Executed before each test
+    }
+
+    @After
+    public void finishTest() throws Exception {
+        // Executed after each test
+    }
+
+    @Test
+    public void t_functionOne() {
+        Assert.assertTrue(true);
+        Assert.assertEquals(2, 2);
+        // All the tests about the function `functionOne`.
+    }
+}
