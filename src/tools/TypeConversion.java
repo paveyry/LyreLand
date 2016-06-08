4b1fece29140289c1187005f2bbecd808da159bc
Pierre-Alexandre VEYRY <veyry_p@epita.fr>
2016-06-08 18:07:59 +0200

    repo: maven: fix all maven warnings

--- src/tools/TypeConversion.java
+++ src/tools/TypeConversion.java
@@ -3,11 +3,9 @@ package tools;
 import java.util.List;
 
 public class TypeConversion {
-    public static int[] convertIntegers(List<Integer> integers)
-    {
+    public static int[] convertIntegers(List<Integer> integers) {
         int[] ret = new int[integers.size()];
-        for (int i = 0; i < ret.length; ++i)
-        {
+        for (int i = 0; i < ret.length; ++i) {
             ret[i] = integers.get(i);
         }
         return ret;
