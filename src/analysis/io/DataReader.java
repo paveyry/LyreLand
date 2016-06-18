51b858e7b18ccefbf6b100dab2acc20fb0970cc2
Pierre-Alexandre VEYRY <veyry_p@epita.fr>
2016-06-18 18:44:33 +0200

    analysis: io: remove IO package and classes since they are deprecated with serialization

--- src/analysis/io/DataReader.java
+++
@@ -1,6 +0,0 @@
-package analysis.io;
-
-public class DataReader {
-    // FIXME: This class should be able to read data from the database
-    // FIXME: and use it for the "learning" algorithm or MarkovChain.
-}
