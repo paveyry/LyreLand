45a82c90bc85f01eab057e3e22dfbf4d3a8401fd
Pierre-Alexandre VEYRY <veyry_p@epita.fr>
2016-06-18 20:03:35 +0200

    main: add threading with thread pool for the midi analysis

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import analysis.ScoreAnalyser;
+import analysis.harmonic.ChordDegreeProcessor;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 import jm.music.data.*;
 import jm.util.Play;
 import jm.util.View;
@@ -5,7 +9,17 @@ import jm.util.Write;
 import tonality.Scale;
 import tonality.Tonality;
 
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
@@ -34,6 +48,33 @@ public class Main {
         if (ExecutionParameters.analyze) {
             // Launch analysis for each file in ExecutionParameters.midDirPath and create training set
             // in ExecutionParameters.trainingSetPath directory.
+            try {
+                double startTime = System.currentTimeMillis();
+                ExecutorService service = Executors.newFixedThreadPool(10);
+                Files.walk(Paths.get(ExecutionParameters.midDirPath))
+                        .filter(Files::isRegularFile)
+                        .filter(s -> s.toString().endsWith(".mid"))
+                        .forEach(s -> /*service.execute(() -> */{
+                            ScoreAnalyser sa = new ScoreAnalyser(s.toString());
+                            sa.processDegreeList();
+                            try {
+                                FileOutputStream fos = new FileOutputStream(s.toString().substring(0, s.toString().length() - 4) + ".xml");
+                                XStream xstream = new XStream(new DomDriver());
+                                fos.write(xstream.toXML(sa).getBytes());
+                            } catch (FileNotFoundException e) {
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+                        }/*)*/);
+                service.shutdown();
+                service.awaitTermination(1, TimeUnit.HOURS);
+                double endTime = System.currentTimeMillis();
+                System.out.println("EXECUTION TIME: " + (endTime - startTime));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         if (ExecutionParameters.train) {
             // Launch training from the training set in ExecutionParameters.trainingSetPath and create
