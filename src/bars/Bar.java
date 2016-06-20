4c2086500459125b746ddda08337f58748227a14
Pierre-Alexandre VEYRY <veyry_p@epita.fr>
2016-06-20 23:29:50 +0200

    bars: add BarNote and Bar classes along with bar lexer and bar-based chord detection

---
+++ src/bars/Bar.java
@@ -0,0 +1,25 @@
+package bars;
+
+import jm.music.data.Note;
+
+import java.util.ArrayList;
+
+public class Bar {
+    private ArrayList<BarNote> notes_;
+
+    public Bar() {
+        this.notes_ = new ArrayList<>();
+    }
+
+    public Bar(ArrayList<BarNote> notes_) {
+        this.notes_ = notes_;
+    }
+
+    public void addNote(BarNote note) {
+        notes_.add(note);
+    }
+
+    public ArrayList<BarNote> getNotes() {
+        return notes_;
+    }
+}
