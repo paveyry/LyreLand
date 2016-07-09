fac47a6dd2aa590c9dd4f8b2ae4f8fda2be84609
Pierre-Alexandre VEYRY <veyry_p@epita.fr>
2016-07-10 00:03:52 +0200

    options: fix default path values

--- src/main/options/ExecutionParameters.java
+++ src/main/options/ExecutionParameters.java
@@ -55,22 +55,22 @@ public class ExecutionParameters {
     /**
      * Specify the path to input mid Files
     */
-    public static Path midDirPath = Paths.get(Misc.getJarPath() + "../assets/midi/");
+    public static Path midDirPath = Paths.get("assets/midi/");
 
     /**
      * Specify the path to the training set (for writing or reading)
      */
-    public static Path trainingSetPath = Paths.get(Misc.getJarPath() + "training/training-set");
+    public static Path trainingSetPath = Paths.get("assets/training/training-set");
 
     /**
      * Specify the path to the trained data (created after training and used for generation)
      */
-    public static Path trainedDataPath = Paths.get(Misc.getJarPath() + "training/trained-data");
+    public static Path trainedDataPath = Paths.get(Misc.getJarPath() + "assets/training/trained-data");
 
     /**
      * Specify the path to the generated files without the extension. Default value is `generated.mid` and `generated.wav`
      */
-    public static String outputPath = Misc.getJarPath() + "../generated";
+    public static String outputPath = Misc.getJarPath() + "generated";
 
 
 }
