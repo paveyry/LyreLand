1eae8179796dd9b1c437f5431c2b5a5759100ecb
Pierre-Alexandre VEYRY <veyry_p@epita.fr>
2016-07-21 16:09:04 +0200

    main: analyze files sorted in descending size order to limit waste of threads in the end

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,8 @@ import main.options.OptionManager;
 
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Comparator;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -41,6 +43,11 @@ public class Main {
                 Files.walk(ExecutionParameters.midDirPath)
                         .filter(Files::isRegularFile)
                         .filter(s -> s.toString().endsWith(".mid"))
+                        .sorted((o1, o2) -> {
+                            File f1 = new File(o1.toString());
+                            File f2 = new File(o2.toString());
+                            return (int) (f2.length() - f1.length());
+                        })
                         .forEach(s -> {
                             TrainingExampleGenerator teg = new TrainingExampleGenerator(s);
                             if (ExecutionParameters.parallel)
