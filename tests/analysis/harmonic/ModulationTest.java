cc09e7cf31682a7f64b5970b0c3b298b1d9ca127
Olivier Laugier <ol.laugier@gmail.com>
2016-10-19 14:59:11 +0200

    tests: add new test in ModulationTest class

--- tests/analysis/harmonic/ModulationTest.java
+++ tests/analysis/harmonic/ModulationTest.java
@@ -59,7 +59,7 @@ public class ModulationTest {
                 ++counter;
         double value = (counter / approximation.size()) * 100;
         boolean result = value >= 85;
-        //System.out.println(value);
+        System.out.println(value);
         Assert.assertTrue(result);
     }
 
@@ -74,15 +74,46 @@ public class ModulationTest {
         Tonality GMajor = new Tonality(1, 0);
         Tonality DMinor = new Tonality(-1, 1);
         DMinor.setMode_(Tonality.Mode.HARMONICMINOR);
+        Tonality DMinorM = new Tonality(-1, 1);
+        DMinorM.setMode_(Tonality.Mode.MELODICMINOR);
         Tonality AMinor = new Tonality(0, 1);
+        AMinor.setMode_(Tonality.Mode.MELODICMINOR);
+        Tonality AMinorH = new Tonality(0, 1);
+        AMinorH.setMode_(Tonality.Mode.HARMONICMINOR);
+        Tonality FMajor = new Tonality(-1, 0);
         ArrayList<Tonality> wantedResult = new ArrayList<>();
-        for (int i = 0; i < 5; ++i)
+        for (int i = 0; i < 6; ++i)
             wantedResult.add(CMajor);
         for (int i = 0; i < 9; ++i)
             wantedResult.add(GMajor);
-        for (int i = 0; i < 3; ++i)
+        for (int i = 0; i < 4; ++i)
             wantedResult.add(CMajor);
         for (int i = 0; i < 2; ++i)
             wantedResult.add(DMinor);
+        for (int i = 0; i < 4; ++i)
+            wantedResult.add(AMinor);
+        wantedResult.add(AMinorH);
+        for (int i = 0; i < 2; ++i)
+            wantedResult.add(CMajor);
+        for (int i = 0; i < 1; ++i)
+            wantedResult.add(DMinorM);
+        for (int i = 0; i < 5; ++i)
+            wantedResult.add(CMajor);
+        for (int i = 0; i < 5; ++i)
+            wantedResult.add(FMajor);
+        for (int i = 0; i < 3; ++i)
+            wantedResult.add(CMajor);
+        ModulationDetector m = new ModulationDetector(sa.getTonality(), sa.getBarLexer());
+        ArrayList<Tonality> approximation = m.computeTonalities();
+        System.out.println(wantedResult);
+        System.out.println(approximation);
+        double counter = 0.0;
+        for (int i = 0; i < approximation.size(); ++i)
+            if (wantedResult.get(i).equals(approximation.get(i)))
+                ++counter;
+        double value = (counter / approximation.size()) * 100;
+        boolean result = value >= 85;
+        System.out.println(value);
+        Assert.assertTrue(result);
     }
 }
