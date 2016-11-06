b890f518b8dd4cb3fdb823a5202db3f03dab8ae5
Olivier Laugier <ol.laugier@gmail.com>
2016-11-06 15:29:44 +0100

    lstm: update ABCIterator class.

--- src/lstm/ABCIterator.java
+++ src/lstm/ABCIterator.java
@@ -1,88 +1,158 @@
 package lstm;
 
+import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.DataSetPreProcessor;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.factory.Nd4j;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.*;
 
 public class ABCIterator implements DataSetIterator {
-    private int miniBatchSize_;
-    private int exempleLength_;
+    private int batchSize_; // Number of lines of the files taken at once by the next() function.
+    private int exampleLength_; // Size of one line of the file.
+    private int numberOfExample_; // Number of line in the file. (1 example = 1 line)
+    private char[] fileChars_; // All the characters from the file.
+    private HashMap<Character, Integer> charToInt_; // HashMap used to convert a Character to an Integer for INDArrays.
+    private Random random_; // Randomize inputs given in a batch.
+    private LinkedList<Integer> exampleStartOffsets_; // All the offsets where a line start in the fileChars_ array.
 
     /**
      * Constructor of DataSetIterator
      * @param filePath
-     * @param miniBatchSize Number of examples taken in one batch by the LSTM
+     * @param batchSize Number of examples taken in one batch by the LSTM
      * @param exampleLength Size of an example (length of a row)
      * @throws IOException
      */
-    public ABCIterator(String filePath, int miniBatchSize, int exampleLength) throws IOException {
+    public ABCIterator(String filePath, Charset fileEncoding, int batchSize, int exampleLength,
+                       HashMap<Character, Integer> charToInt, Random random) throws IOException {
         // Check if given dataFile exist.
         if( !new File(filePath).exists())
             throw new IOException("Could not access file (does not exist): " + filePath);
         // Check miniBatchSize value > 0
-        if( miniBatchSize <= 0 )
+        if( batchSize <= 0 )
             throw new IllegalArgumentException("Invalid miniBatchSize strictly positive");
 
-        miniBatchSize_ = miniBatchSize;
-        exempleLength_ = exampleLength;
+        batchSize_ = batchSize;
+        exampleLength_ = exampleLength;
+        charToInt_ = charToInt;
+        random_ = random;
+        exampleStartOffsets_ = new LinkedList<>();
+
+        List<String> lines = Files.readAllLines(new File(filePath).toPath(), fileEncoding); // Remove '\n' ...
+        numberOfExample_ = lines.size();
+        char[] characters = new char[(numberOfExample_ * exampleLength_) + numberOfExample_]; // To add missing '\n'
+        int index = 0;
+        for(String line : lines ){
+            char[] currLine = line.toCharArray();
+            for (char c : currLine)
+                characters[index++] = c;
+            characters[index++] = '\n';
+        }
+        fileChars_ = characters;
+        initializeOffsets();
     }
 
+    // PRIVATE METHODS
+
+    /**
+     * This function fill the exampleStartOffsets_ linkedList with the start values of each line
+     * and shuffle them according to random_.
+     */
+    private void initializeOffsets() {
+        // Initialize all offset and shuffle them according to random_.
+        for (int i = 0; i < numberOfExample_; i++) {
+            exampleStartOffsets_.add(i * exampleLength_);
+        }
+        Collections.shuffle(exampleStartOffsets_, random_);
+    }
+
+    // PUBLIC METHODS
     @Override
-    public DataSet next(int i) {
-        return null;
+    public boolean hasNext() {
+        return exampleStartOffsets_.size() > 0;
     }
 
     @Override
     public DataSet next() {
-        return null;
+        return this.next(batchSize_);
     }
 
+    /**
+     * This function fill two arrays (input/output) with the next N examples
+     * in order to create the dataSet given to the LSTM.
+     * @param number
+     * @return
+     */
     @Override
-    public boolean hasNext() {
-        return false;
+    public DataSet next(int number) {
+        if(exampleStartOffsets_.size() == 0)
+            throw new NoSuchElementException();
+
+        int currBatchSize = Math.min(number, exampleStartOffsets_.size());
+
+        // Order of the INDArray.
+        // dimension 0 = number of examples in batch
+        // dimension 1 = size of each vector (i.e., number of characters)
+        // dimension 2 = length of each time series/example
+        INDArray input = Nd4j.create(new int[]{currBatchSize, charToInt_.size(), exampleLength_}, 'f');
+        INDArray labels = Nd4j.create(new int[]{currBatchSize, charToInt_.size(), exampleLength_}, 'f');
+
+        for(int i = 0; i<currBatchSize; i++){
+            int startIndex = exampleStartOffsets_.removeFirst();
+            int endIndex = startIndex + exampleLength_;
+            int currCharIndex = charToInt_.get(fileChars_[startIndex]);	//Current input
+            int c = 0;
+            for(int j = startIndex+1; j < endIndex; j++, c++){
+                int nextCharIndex = charToInt_.get(fileChars_[j]); //Next character to predict
+                input.putScalar(new int[]{i, currCharIndex, c}, 1.0);
+                labels.putScalar(new int[]{i, nextCharIndex, c}, 1.0);
+                currCharIndex = nextCharIndex;
+            }
+        }
+        return new DataSet(input, labels);
     }
 
+    // Number of possible
     @Override
     public int totalExamples() {
-        return 0;
+        return numberOfExample_;
     }
 
     @Override
     public int inputColumns() {
-        return 0;
+        return charToInt_.size();
     }
 
     @Override
     public int totalOutcomes() {
-        return 0;
+        return charToInt_.size();
     }
-
 
     @Override
     public void reset() {
-
-    }
-
-    @Override
-    public int batch() {
-        return 0;
+        exampleStartOffsets_.clear();
+        initializeOffsets();
     }
 
     @Override
     public int cursor() {
-        return 0;
+        return totalExamples() - exampleStartOffsets_.size();
     }
 
     @Override
     public int numExamples() {
-        return 0;
+        return totalExamples();
     }
 
-    // Basic or Unimplemented Methods
+    @Override
+    public int batch() {
+        return batchSize_;
+    }
 
     @Override
     public boolean resetSupported() {
@@ -93,6 +163,8 @@ public class ABCIterator implements DataSetIterator {
     public boolean asyncSupported() {
         return true;
     }
+
+    // UNIMPLEMENTED METHODS (not used ...)
 
     @Override
     public void setPreProcessor(DataSetPreProcessor dataSetPreProcessor) {
