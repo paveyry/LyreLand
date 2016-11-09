f72252a534c67d086b0cfdfe0cdf5bd268943e30
Pierre-Alexandre VEYRY <veyry_p@epita.fr>
2016-11-09 17:16:08 +0100

    tests: lstm: add lstm training test

---
+++ tests/main/LSTMTest.java
@@ -0,0 +1,25 @@
+package main;
+
+import lstm.LSTMTrainer;
+import main.options.ExecutionParameters;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import tools.Misc;
+import website.java.app.util.ResourceManager;
+
+import java.io.IOException;
+
+public class LSTMTest {
+    @BeforeClass
+    public static void initClass() {
+        ExecutionParameters.verbose = false;
+    }
+
+    @Test
+    public void lstmTest() throws IOException {
+        LSTMTrainer trainer = new LSTMTrainer(Misc.getProjectPath() + "assets/abc/data.abc", 3657263);
+        trainer.train();
+        trainer.serialize(Misc.getProjectPath() + "serialized_lstm");
+    }
+
+}
