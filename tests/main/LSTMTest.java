b152744e836988b12736d221746dfb55373cc72d
Pierre-Alexandre VEYRY <veyry_p@epita.fr>
2016-12-09 17:57:23 +0100

    experiments: add simple abc2midi parsing in LSTMtest

--- tests/main/LSTMTest.java
+++ tests/main/LSTMTest.java
@@ -1,5 +1,11 @@
 package main;
 
+import abc.midi.BasicMidiConverter;
+import abc.midi.MidiConverterAbstract;
+import abc.notation.Tune;
+import abc.parser.AbcFileParser;
+import abc.parser.AbcTune;
+import abc.parser.AbcTuneBook;
 import lstm.ABCIterator;
 import lstm.LSTMTrainer;
 import main.options.ExecutionParameters;
@@ -8,8 +14,14 @@ import org.junit.Test;
 import tools.Misc;
 import website.java.app.util.ResourceManager;
 
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Track;
+import javax.sound.midi.spi.MidiFileWriter;
+import java.io.File;
 import java.io.IOException;
 import java.util.Random;
+import java.util.Set;
 
 public class LSTMTest {
     @BeforeClass
@@ -19,9 +31,19 @@ public class LSTMTest {
 
     @Test
     public void lstmTest() throws IOException {
-        LSTMTrainer trainer = null;
-        trainer = LSTMTrainer.deserialize(Misc.getProjectPath() + "lstm-epoch-98-lr0.04", "assets/abc/nottingham/database.abc");
-        System.out.println(trainer.generate();
+        AbcFileParser parser = new AbcFileParser();
+        AbcTuneBook b = parser.parse(new File("i.abc"));
+        BasicMidiConverter converter = new BasicMidiConverter();
+        Tune t = null;
+        for (Object i : b.getTunes().keySet()) {
+            t = (Tune) b.getTunes().get(i);
+        }
+        Sequence s = converter.toMidiSequence(t);
+        MidiSystem.write(s, 1, new File("i.mid"));
+        LSTMTrainer trainer = LSTMTrainer.deserialize(Misc.getProjectPath()
+                + "assets/training/trained-lstm/nottingham/trained-lstm.bin",
+                "assets/abc/nottingham/database.abc");
+        System.out.println(trainer.generate());
         //trainer.generateSamples(1, 1, 25);
     }
 
